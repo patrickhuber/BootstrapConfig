576d8aac8bb13d4de47d4f5d39f846c62a39f2b4
Patrick Huber <prhuber@gmail.com>
2013-08-10 12:47:40 -0400

    adds initial revision

---
+++ BootstrapConfig.Core/BootstrapConfigurationSection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace BootstrapConfig
+{
+    /// <summary>
+    /// defines the client config bootstrap configuration sections
+    /// </summary>
+    public sealed class BootstrapConfigurationSection : ConfigurationSection, IBoostrapConfiguration
+    {
+        private const string KeyKey = "key";
+
+        /// <summary>
+        /// Gets or sets the key.
+        /// </summary>
+        /// <value>
+        /// The key.
+        /// </value>
+        public string Key
+        { 
+            get { return (string)this[KeyKey]; }
+            set { this[KeyKey] = value; }
+        }
+    }
+}
---
+++ BootstrapConfig.WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BootstrapConfig.WebApp.MvcApplication" Language="C#" %>
