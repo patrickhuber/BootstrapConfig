d14fccc98ba9ad3b056891abe8ba20a56423c6e9
Patrick Huber <prhuber@gmail.com>
2014-01-29 12:16:33 -0500

    adds configuration provider to abstractions

---
+++ BootstrapConfig.Core/Abstractions/IConfigurationProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+
+namespace BootstrapConfig.Abstractions
+{
+    public interface IConfigurationProvider
+    {
+        NameValueCollection AppSettings { get; }
+    }
+}
